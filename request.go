62de075e67a575deb97eda582f3c4aac905aa0ab
Jota Santos <jamillo@gmail.com>
2024-03-09 19:43:35 -0300

    feat: Add Redirect to Response (#2)

--- request.go
+++ request.go
@@ -1,9 +1,5 @@
 package lambda
 
-import (
-	"encoding/json"
-)
-
 type Query = multiValues
 
 type Headers = multiValues
@@ -18,53 +14,4 @@ type Request[T any] struct {
 	Query      Query
 	Headers    Headers
 	Body       T
-}
-
-func (r *Response[T]) Status(status int) *Response[T] {
-	r.status = status
-	return r
-}
-
-func (r *Response[T]) Header(key string, value string) *Response[T] {
-	if r.header == nil {
-		r.header = make(map[string][]string)
-	}
-	h, ok := r.header[key]
-	if !ok {
-		r.header[key] = []string{value}
-		return r
-	}
-	r.header[key] = append(h, value)
-	return r
-}
-
-func (r *Response[T]) JSON(data any) *Response[T] {
-	r.header["Content-Type"] = []string{"application/json"}
-	if r.body.Len() > 0 {
-		r.body.Reset()
-	}
-	err := json.NewEncoder(&r.body).Encode(data)
-	if err != nil {
-		r.err = err
-	}
-	return r
-}
-
-func (r *Response[T]) SendString(data string) *Response[T] {
-	if r.body.Len() > 0 {
-		r.body.Reset()
-	}
-
-	err := json.NewEncoder(&r.body).Encode(data)
-	if err != nil {
-		r.err = err
-	}
-	return r
-}
-
-func (r *Response[T]) Error() string {
-	if r.err != nil {
-		return r.err.Error()
-	}
-	return ""
 }
--- response.go
+++ response.go
@@ -2,6 +2,7 @@ package lambda
 
 import (
 	"bytes"
+	"encoding/json"
 )
 
 type Response[T any] struct {
@@ -10,4 +11,58 @@ type Response[T any] struct {
 	body   bytes.Buffer
 	err    error
 	Body   T
+}
+
+func (r *Response[T]) Redirect(url string, status int) {
+	r.status = status
+	r.header["Location"] = []string{url}
+}
+
+func (r *Response[T]) Status(status int) *Response[T] {
+	r.status = status
+	return r
+}
+
+func (r *Response[T]) Header(key string, value string) *Response[T] {
+	if r.header == nil {
+		r.header = make(map[string][]string)
+	}
+	h, ok := r.header[key]
+	if !ok {
+		r.header[key] = []string{value}
+		return r
+	}
+	r.header[key] = append(h, value)
+	return r
+}
+
+func (r *Response[T]) JSON(data any) *Response[T] {
+	r.header["Content-Type"] = []string{"application/json"}
+	if r.body.Len() > 0 {
+		r.body.Reset()
+	}
+	err := json.NewEncoder(&r.body).Encode(data)
+	if err != nil {
+		r.err = err
+	}
+	return r
+}
+
+func (r *Response[T]) SendString(data string) *Response[T] {
+	if r.body.Len() > 0 {
+		r.body.Reset()
+	}
+
+	err := json.NewEncoder(&r.body).Encode(data)
+	if err != nil {
+		r.err = err
+	}
+	return r
+}
+
+func (r *Response[T]) Error() string {
+	if r.err != nil {
+		return r.err.Error()
+	}
+	return ""
 }
