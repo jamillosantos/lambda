33897c01bfd2236685924c8d40c3222fd8a4b666
Jota Santos <jamillo@gmail.com>
2024-03-09 18:04:17 -0300

    feat!: Add support to Query, Header and Path variables (#1)

--- context.go
+++ context.go
@@ -1,60 +1,20 @@
 package lambda
 
 import (
-	"bytes"
-	"encoding/json"
+	"context"
 )
 
-type Context[R any] struct {
-	Request R
-	status  int
-	header  map[string][]string
-	body    bytes.Buffer
-	error   error
+// None is an empty struct used when we are not interested on the request or response body.
+type None struct{}
+
+type Context[Req any, Resp any] struct {
+	Context  context.Context
+	Request  Request[Req]
+	Response Response[Resp]
+	error    error
 }
 
-func (l *Context[R]) Status(status int) *Context[R] {
-	l.status = status
-	return l
-}
-
-func (l *Context[R]) Header(key string, value string) *Context[R] {
-	if l.header == nil {
-		l.header = make(map[string][]string)
-	}
-	h, ok := l.header[key]
-	if !ok {
-		l.header[key] = []string{value}
-		return l
-	}
-	l.header[key] = append(h, value)
-	return l
-}
-
-func (l *Context[R]) JSON(data any) *Context[R] {
-	l.header["Content-Type"] = []string{"application/json"}
-	if l.body.Len() > 0 {
-		l.body.Reset()
-	}
-	err := json.NewEncoder(&l.body).Encode(data)
-	if err != nil {
-		l.error = err
-	}
-	return l
-}
-
-func (l *Context[R]) SendString(data string) *Context[R] {
-	if l.body.Len() > 0 {
-		l.body.Reset()
-	}
-	_, err := l.body.WriteString(data)
-	if err != nil {
-		l.error = err
-	}
-	return l
-}
-
-func (l *Context[R]) Error() string {
+func (l *Context[Req, Resp]) Error() string {
 	if l.error != nil {
 		return l.error.Error()
 	}
--- start.go
+++ start.go
@@ -1,82 +1,82 @@
 package lambda
 
 import (
-	"bytes"
 	"context"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 // Start will start the lambda function with the given handler and options.
 //
 // The handler is a function that receives a context and a pointer to a Context.
-func Start[Req any](handler func(context.Context, *Context[Req]) error, opts ...Option) {
+func Start[Req any, Resp any](handler func(*Context[Req, Resp]) error, opts ...Option) {
 	c := defaultOpts()
 	for _, o := range opts {
 		o(&c)
 	}
 
-	lambda.Start(func(ctx context.Context, gatewayReq events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-		lambdaContext := Context[Req]{
-			status: http.StatusOK,
+	lambda.Start(func(ctx context.Context, gatewayReq APIGatewayProxyRequest) (APIGatewayProxyResponse, error) {
+		lambdaContext := Context[Req, Resp]{
+			Context: ctx,
+			Request: Request[Req]{
+				req:        &gatewayReq,
+				HTTPMethod: gatewayReq.HTTPMethod,
+				Path:       gatewayReq.Path,
+				PathParams: gatewayReq.PathParameters,
+				Query: Query{
+					mapUtils:      gatewayReq.QueryStringParameters,
+					mapArrayUtils: gatewayReq.MultiValueQueryStringParameters,
+				},
+				Headers: Headers{
+					mapUtils:      gatewayReq.Headers,
+					mapArrayUtils: gatewayReq.MultiValueHeaders,
+				},
+			},
+			Response: Response[Resp]{
+				status: http.StatusOK,
+			},
 		}
 
 		// For the string -> []byte we need to use a more effective way. For now, let's keep the naive approach.
-		err := unmarshalBody(&gatewayReq, &lambdaContext)
+		err := populateLambdaContext(&gatewayReq, &lambdaContext)
 		if err != nil {
 			return c.errorHandler(err)
 		}
 
-		err = handler(ctx, &lambdaContext)
-		if !errors.Is(err, &lambdaContext) && err != nil {
+		err = handler(&lambdaContext)
+		if !(errors.Is(err, &lambdaContext.Response)) && err != nil {
 			lambdaContext.error = err
 		}
-		if lambdaContext.error != nil {
-			return c.errorHandler(lambdaContext.error)
+		if lambdaContext.Response.err != nil {
+			return c.errorHandler(lambdaContext.Response.err)
 		}
-		fmt.Println("lambdaContext.status", lambdaContext.status)
-		fmt.Println("lambdaContext.header", lambdaContext.header)
-		fmt.Println("lambdaContext.body.String()", lambdaContext.body.String())
 
-		return events.APIGatewayProxyResponse{
-			StatusCode:        lambdaContext.status,
-			MultiValueHeaders: lambdaContext.header,
-			Body:              lambdaContext.body.String(),
-			IsBase64Encoded:   false,
-		}, nil
+		r := APIGatewayProxyResponse{
+			StatusCode:        lambdaContext.Response.status,
+			MultiValueHeaders: lambdaContext.Response.header,
+			Body:              lambdaContext.Response.body.Bytes(),
+		}
+		return r, nil
 	})
 }
 
-// unmarshalBody will unmarshal the body from the gateway request into the lambda context.
-func unmarshalBody[Req any](gatewayReq *events.APIGatewayProxyRequest, lambdaContext *Context[Req]) error {
-	fmt.Println("gatewayReq.HTTPMethod", gatewayReq.HTTPMethod)
+// populateLambdaContext will unmarshal the body from the gateway request into the lambda context.
+func populateLambdaContext[Req any, Resp any](gatewayReq *APIGatewayProxyRequest, lambdaContext *Context[Req, Resp]) error {
 	if gatewayReq.HTTPMethod == "GET" {
 		return nil
 	}
-	fmt.Println("len(gatewayReq.Body)", len(gatewayReq.Body))
 	if len(gatewayReq.Body) == 0 {
 		return nil
 	}
-	var reader io.Reader
-	fmt.Println("gatewayReq.IsBase64Encoded", gatewayReq.IsBase64Encoded)
+	var reader io.Reader = strings.NewReader(gatewayReq.Body)
 	if gatewayReq.IsBase64Encoded {
-		err := json.NewDecoder(
-			base64.NewDecoder(
-				base64.StdEncoding, bytes.NewReader([]byte(gatewayReq.Body)),
-			),
-		).Decode(&lambdaContext)
-		if err != nil {
-			return err
-		}
-	} else {
-		reader = bytes.NewReader([]byte(gatewayReq.Body))
+		reader = base64.NewDecoder(base64.StdEncoding, reader)
 	}
-	return json.NewDecoder(reader).Decode(lambdaContext.Request)
+	return json.NewDecoder(reader).Decode(&lambdaContext.Request.Body)
 }
