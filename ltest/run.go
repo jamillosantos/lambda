b44daa6a18082a5b562f922ec7c635de365fc1d3
Jamillo Santos <jota@setare.io>
2024-03-16 16:16:46 -0300

    feat: Introduce TestContext

--- ltest/run.go
+++ ltest/run.go
@@ -2,12 +2,26 @@ package ltest
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 
 	"github.com/jamillosantos/lambda"
 )
 
-func Run[Req any, Resp any](handler lambda.Handler[Req, Resp], opts ...Option) (*lambda.Context[Req, Resp], error) {
+type TestContext[Req any, Resp any] struct {
+	lambda.Context[Req, Resp]
+}
+
+func (t *TestContext[Req, Resp]) ResponseBody() (Resp, error) {
+	var r Resp
+	err := json.NewDecoder(&t.Response.Body).Decode(&r)
+	if err != nil {
+		return r, err
+	}
+	return r, nil
+}
+
+func Run[Req any, Resp any](handler lambda.Handler[Req, Resp], opts ...Option) (*TestContext[Req, Resp], error) {
 	o := options{
 		httpMethod: "GET",
 		path:       "/",
@@ -20,23 +34,25 @@ func Run[Req any, Resp any](handler lambda.Handler[Req, Resp], opts ...Option) (
 	for _, opt := range opts {
 		opt(&o)
 	}
-	ctx := &lambda.Context[Req, Resp]{
-		Context: context.Background(),
-		Request: lambda.Request[Req]{
-			HTTPMethod: o.httpMethod,
-			Path:       o.path,
-			PathParams: o.pathParams,
-			Query:      lambda.NewQuery(make(map[string]string), o.query),
-			Headers:    lambda.NewHeaders(make(map[string]string), o.headers),
-			Body:       o.req.(Req),
+	ctx := &TestContext[Req, Resp]{
+		lambda.Context[Req, Resp]{
+			Context: context.Background(),
+			Request: lambda.Request[Req]{
+				HTTPMethod: o.httpMethod,
+				Path:       o.path,
+				PathParams: o.pathParams,
+				Query:      lambda.NewQuery(make(map[string]string), o.query),
+				Headers:    lambda.NewHeaders(make(map[string]string), o.headers),
+				Body:       o.req.(Req),
+			},
+			Response: lambda.Response[Resp]{
+				StatusCode: http.StatusOK,
+				Headers:    make(map[string][]string),
+			},
+			Locals: o.locals,
 		},
-		Response: lambda.Response[Resp]{
-			StatusCode: http.StatusOK,
-			Headers:    make(map[string][]string),
-		},
-		Locals: o.locals,
 	}
-	err := handler(ctx)
+	err := handler(&ctx.Context)
 	if err == &ctx.Response { // nolint
 		if ctx.Response.Err != nil {
 			return nil, ctx.Response.Err
